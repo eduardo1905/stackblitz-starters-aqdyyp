a8c7005c765763f33b45371547636df281aa01f0
eduardo1905 <128932100+eduardo1905@users.noreply.github.com>
2023-06-09 17:10:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-aqdyyp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-aqdyyp)
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const handleCompleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleCompleteTask(index)}>Complete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
